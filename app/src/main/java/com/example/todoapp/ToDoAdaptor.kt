9a410338735ed3738ce8d124ae963493b474f4a5
i5awlah <91419314+i5awlah@users.noreply.github.com>
2021-11-30 15:30:07 +0300

    update

--- app/src/main/java/com/example/todoapp/ToDoAdaptor.kt
+++ app/src/main/java/com/example/todoapp/ToDoAdaptor.kt
@@ -5,22 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.item_row.view.*
+import com.example.todoapp.databinding.ItemRowBinding
 
 
 class ToDoAdaptor(private val items: ArrayList<ToDoItem>) : RecyclerView.Adapter<ToDoAdaptor.ItemViewHolder>() {
-    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class ItemViewHolder(val binding: ItemRowBinding): RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         return ItemViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.item_row, parent, false)
+            ItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         )
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = items[position]
 
-        holder.itemView.apply {
+        holder.binding.apply {
             tvItem.text = item.content
             checkBox.isChecked = item.checked
             checkBox.setOnClickListener{
